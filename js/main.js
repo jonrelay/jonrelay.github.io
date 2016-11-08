081457061c3e41713031298da0b186d23bc8f91f
Jon Reid <jonreid.games+relay@gmail.com>
2016-11-08 01:00:12 +0000

    Screenshotty thing

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 $(function() {
+
+showSlides(1);
+
     var controller = new ScrollMagic.Controller({
 			globalSceneOptions: {
 				triggerHook: 'onLeave'
@@ -27,3 +30,30 @@ var scene = new ScrollMagic.Scene({triggerElement: "#scrollTrigger1", duration: 
 
 
 });
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
